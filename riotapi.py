cb0dd21a24d28d1567ecb8b0f0cbac2989abad5e
epa123 <yanpitangui@gmail.com>
2015-11-17 17:50:39 -0200

    api da riot em python
    o resultado de partida ativa está muito lento

--- riotapi.py
+++ riotapi.py
@@ -25,7 +25,7 @@
 import requests
 
 global apikey
-apikey = 'da52e9a5-240d-4cac-9538-7ae90c298bb3'
+apikey = '01b0e541-ea0b-44a6-8207-97fcabcb6b0e'
 
 def searchbysumm(invocador,regiao):
 	invocador = invocador.replace(' ','')
@@ -37,32 +37,79 @@ def searchbysumm(invocador,regiao):
 	if jsrequest.status_code == 404:
 		return None
 	resposta = jsrequest.json()
-	return resposta[invocador.lower()]['id'], resposta[invocador.lower()]['name']
+	return resposta[invocador.lower()]['id'], resposta[invocador.lower()]['name'] #retorna o id do invocador e o nome real
 	
-def getelo(sumid,regiao,nome):
+def getelo(sumid,regiao):
 	poss = ['BR','EUW','EUNE','NA','TR','OCE','CN','KR','LAN','RU','SEA','LAS']
 	if regiao.upper() not in poss:
 		return None
-	url = 'https://br.api.pvp.net/api/lol/%s/v2.5/league/by-summoner/%s/entry?api_key=%s'% (regiao.lower(), sumid, apikey)
+	url = 'https://%s.api.pvp.net/api/lol/%s/v2.5/league/by-summoner/%s/entry?api_key=%s'% (regiao.lower(),regiao.lower(), sumid, apikey)
 	jsrequest = requests.get(url)
 	if jsrequest.status_code in  [400,401,404,429,500,503]:
 		return None
 	resposta = jsrequest.json()
 	if resposta[str(sumid)]== []:
+		return (UNRANKED,0,0)
+	return (str(resposta[str(sumid)][0]['tier']), str(resposta[str(sumid)][0]['entries'][0]['division']), str(resposta[str(sumid)][0]['entries'][0]['leaguePoints'])) #alterado para retorno de dados puros
+	
+def partidaativa(sumid,regiao): #a api da riot precisa da região e do summoner ID, por isso não é possivel o usuário chamá-la manualmente
+	dicreg = {'br':'BR1', 'na':'NA1', 'lan':'LA1', 'las':'LA2', 'oce':'OC1', 'eune':'EUN1', 'tr':'TR1', 'ru':'RU', 'euw':'EUW1', 'kr':'KR'}
+	if regiao.lower() not in dicreg:
 		return None
-	strretorno = 'Invocador: %s\n' % nome
-	strretorno+= 'Liga: %s' % resposta[str(sumid)][0]['tier']
-	strretorno+= ' %s' % resposta[str(sumid)][0]['entries'][0]['division']
-	strretorno+= ' %s PDL' % resposta[str(sumid)][0]['entries'][0]['leaguePoints']
-	return strretorno
+	url = 'https://%s.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/%s/%s?api_key=%s' %(regiao.lower(),dicreg[regiao.lower()],sumid,apikey)
+	jsrequest = requests.get(url)
+	if jsrequest.status_code !=200:
+		return "O jogador não está em partida ativa ou a API da RIOT caiu"
+	resposta = jsrequest.json()
+	strretorno = 'INFORMAÇÕES DE PARTIDA\n'
+	strretorno += 'MAPA: %s\t TIPO DE JOGO: %s\n' %(resposta["gameMode"], resposta['gameType'])
+	if resposta['bannedChampions']!=[]:
+			pass
+	strretorno += '\t\tPARTICIPANTES\n'
+	strtimevermelho = '\tTIME VERMELHO\n'
+	strtimeroxo ='\tTIME ROXO\n'
+	for i in resposta['participants']:
+		if i['teamId']==100:
+			if getelo(i['summonerId'],regiao)[0] == 'UNRANKED':
+				strtimevermelho+= '%s\t' % i['summonerName']
+				strtimevermelho+= '%s\t' % getelo(i['summonerId'],regiao.lower())[0]
+				strtimevermelho+= 'Campeão: %s\n' % _getchampionname(i['championId'],regiao)
+			else:
+				strtimevermelho+= '%s' % i['summonerName']
+				strtimevermelho+= ' %s' % getelo(i['summonerId'],regiao.lower())[0]
+				strtimevermelho+= ' %s' % getelo(i['summonerId'] ,regiao.lower())[1]
+				strtimevermelho+= ' %s PDL\t' % getelo(str(i['summonerId']),regiao.lower())[2]
+				strtimevermelho+= 'Campeão: %s\n' % _getchampionname(i['championId'],regiao)
+		if i['teamId']==200:
+			if getelo(i['summonerId'],regiao)[0] == 'UNRANKED':
+				strtimeroxo+= '%s\t' % i['summonerName']
+				strtimeroxo+= '%s\t' % getelo(i['summonerId'],regiao.lower())[0]
+				strtimeroxo+= 'Campeão: %s\n' % _getchampionname(i['championId'],regiao)
+			else:
+				strtimeroxo+= '%s' % i['summonerName']
+				strtimeroxo+= ' %s' % getelo(i['summonerId'],regiao.lower())[0]
+				strtimeroxo+= ' %s' % getelo(i['summonerId'] ,regiao.lower())[1]
+				strtimeroxo+= ' %s PDL\t' % getelo(str(i['summonerId']),regiao.lower())[2]
+				strtimeroxo+= 'Campeão: %s\n' % _getchampionname(i['championId'],regiao)
+	strretorno+= strtimevermelho+strtimeroxo
+		
+		
+	return strretorno #pensando se retornarei uma string montada ou dados puros(matriz com nome de invocador, tier, divisão pdl e campeão)
 	
-	
+
+def _getchampionname(cmpid,regiao): #só deve ser utilizada por outras funções, visto que ninguem terá o id do campeão manualmente
+	url = 'https://global.api.pvp.net/api/lol/static-data/%s/v1.2/champion/%s?api_key=%s' % (regiao.lower(), cmpid, apikey)
+	jsrequest = requests.get(url)
+	if jsrequest.status_code!=200:
+		return None
+	return jsrequest.json()['name']
+
 
 def main(args):
 	a = input('Qual o nome do invocador a ser pesquisado? ')
 	b = input('Qual a região a ser pesquisada? ')
 	valor = searchbysumm(a,b)
-	print(getelo(valor[0],b,valor[1]))
+	print(partidaativa(valor[0],b))  
 	return 0
 
 if __name__ == '__main__':
